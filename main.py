6d7141bd8e1d917bdaf7ab8e5fa0af466b375bf0
Hoang Duc <duckluvmyself@gmail.com>
2026-08-12 19:01:42 +0700

    feat: modify and adjust test db

--- main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-"""Entry point for video search agent."""
-import uvicorn
-from backend.api.main import app
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
